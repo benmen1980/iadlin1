a7d7d5a5403c6f2db04eff711516424b51822dad
Roy BM <benmen1@gmail.com>
2018-07-27 10:54:06 +0300

    הצלחתי לעדכן תעודת משלוח קיימת בעזרת PATCH

--- include/PriorityRestAPI.php
+++ include/PriorityRestAPI.php
@@ -1,16 +1,17 @@
 <?php
 $login = 'demo';
 $password = '123456';
+$priform = "DOCUMENTS_D";
 
 //next example will recieve all messages for specific conversation
-$service_url = 'https://devpri.roi-holdings.com/odata/priority/tabula.ini/demo/DOCUMENTS_D';
+$service_url = 'https://devpri.roi-holdings.com/odata/priority/tabula.ini/demo/'.$priform;
 $curl = curl_init($service_url);
-//curl_escape($curl,$service_url);
+
 curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
 curl_setopt($curl, CURLOPT_USERPWD, "$login:$password");
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($curl, CURLOPT_URL, str_replace(" ", '%20',$service_url));
-//curl_setopt($curl, CURLOPT_HTTPHEADER, array('application/json;odata.metadata=minimal','Authorization: Basic '. base64_encode("user:password")));
+
 $curl_response = curl_exec($curl);
 if ($curl_response === false) {
 	throw new Exception(curl_error($curl), curl_errno($curl));
@@ -24,9 +25,5 @@ if (isset($decoded->response->status) && $decoded->response->status == 'ERROR') 
 	die('error occured: ' . $decoded->response->errormessage);
 }
 $parsed = $decoded['value'];
-//parse_str(curl_exec($curl), $parsed);
 echo json_encode($parsed);
-//echo $decoded;
-//var_export($decoded->response);
-
 ?>
