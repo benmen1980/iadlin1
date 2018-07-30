a726dbaf754e0998040c2c7466df542fbecc9e06
רועי <benmen1@gmail.com>
2018-07-30 22:50:15 +0300

    המערכת עובדת, גם קריאה וגם עדכון נתונים

---
+++ include/APIconfig.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: רועי
+ * Date: 29/07/2018
+ * Time: 17:25
+ */
+
+
+$login = 'shir';
+$password = '305559593';
+$companyname= 'ar1301';
+$priform = "DOCUMENTS_D";
+
+?>
